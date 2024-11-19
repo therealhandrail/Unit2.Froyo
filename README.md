04d81a41bf5350695b3640035c2a352bfdaa7886
root <root@BrettsPC.localdomain>
2024-11-18 20:53:31 -0500

    functionality

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Unit2.Froyo
 Unit2.Froyo
+
+A visitor receives a prompt upon opening the website to enter a list of comma-separated froyo flavors. They type vanilla,vanilla,vanilla,strawberry,coffee,coffee. When they view the browser console, they observe a table listing how many of each flavor they have ordered. In this case, they will be able to observe that they have ordered three vanilla, two coffee, and one strawberry froyo.
+
+There is no starter code for this workshop; the solutions to previous workshops will be great to review!
+
+Create a new repository named Unit2.Froyo on GitHub and clone it down.
+Open the project with VS Code and create an HTML file and a JavaScript file.
+Figure out a plan for what code you'll need to write based on the user story. Use the rubric to guide you.
+
+<!-- Can you live serve the HTML file?
+Is the HTML file connected to the JS file?
+Can you prompt the user for froyo flavors and store their result? -->
+
+<!-- Can you parse the user input into an array of froyo flavors? -->
+
+Can you build an object to track which flavors you have observed so far?
+
+As you iterate through the array of flavors, when should that object be updated?
+
+Is your logic organized into a function?
---
+++ index.js
@@ -0,0 +1,52 @@
+let userInput = prompt(
+    'Please enter what you would like to order, each item separated by commas.'
+)
+
+const flavor = {
+    type: userInput,
+}
+
+// using the user Input to give attributes to properties, then converting object into array
+
+for(const type in flavor){
+    console.log('type: ',flavor[type]);
+}
+
+const type = Object.values(flavor);
+
+// splitting and parsing array, pushing new items into array
+
+const flavorTypes = userInput.split(",");
+
+for (let i = 0; i < flavor.length; i++) {
+    const str = flavor[i];
+    const number = parseInt(str);
+    flavor.push(type);
+}
+
+console.log(flavorTypes)
+
+
+// I found this code from https://dinma.medium.com/counting-duplicate-values-in-javascript-arrays-b2432f64edf9
+// I changed it to work with this to count each item
+
+// This code takes the items from the array above, dumps it into an empty Object,
+// then assigns the value of 1 to each item, if it find more than 1 of an item,
+// it adds 1 to the value, keeping track of how many times something is in the array.
+// after the values are assigned, it returns them so they can be console logged as an Object.
+
+// might not be the best explanation but I understand what its doing lmao
+
+const countItems = (flavorTypes) => {
+    const counts = {};
+    flavorTypes.forEach((type) => {
+        if (!counts[type]){
+            counts[type] = 1;
+        } else {
+            counts[type]++;
+        }
+    });
+    return counts;
+}
+const result = countItems(flavorTypes);
+console.log(result);
